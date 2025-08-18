1f869f50e870571e7db7dc02f75e1ac94d57bd84
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-08-18 11:35:37 +0500

    feat: create AppConfig to store ARGear API keys and configuration

---
+++ android/app/src/main/java/com/argearproject/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.argearproject
+
+object AppConfig {
+    const val API_URL = "https://apis.argear.io/api/v3/"      // Replace with actual API URL
+    const val API_KEY = "5d89f29b5d4252dd1380c7f2"                  // Replace with actual API key
+    const val SECRET_KEY = "e78d5e2de0001af090bcfd71854ff052fe8280428e000555132cf1e8dda7551d"            // Replace with actual secret key
+    const val AUTH_KEY = "U2FsdGVkX19X1P8SNljd3u62dIEGd6SoJ1kxw8zYA7LZLI9bZcakRF2xOEXTJRGIXm/lZmmglOp7ej7eYoRLsQ=="                // Replace with actual auth key
+}
