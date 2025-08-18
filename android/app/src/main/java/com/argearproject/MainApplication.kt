a4505055f012f94cc4588f447b593b2a1c5c315b
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-08-18 11:37:56 +0500

    chore: register ARGearBridgePackage in MainApplication

--- android/app/src/main/java/com/argearproject/MainApplication.kt
+++ android/app/src/main/java/com/argearproject/MainApplication.kt
@@ -1,38 +1,32 @@
+// android/app/src/main/java/com/argearproject/MainApplication.kt
 package com.argearproject
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.PackageList            // <-- add this
+import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping // <-- keep this
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+  override val reactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+    override fun getUseDeveloperSupport() = BuildConfig.DEBUG
 
-        override fun getJSMainModuleName(): String = "index"
+    // Use PackageList so all autolinked modules (e.g., SafeAreaContext) are included
+    override fun getPackages(): List<ReactPackage> {
+      val packages = PackageList(this).packages
+      packages.add(ARGearBridgePackage()) // keep your custom bridge
+      return packages
+    }
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+    override fun getJSMainModuleName() = "index"
+  }
 
   override fun onCreate() {
     super.onCreate()
-    loadReactNative(this)
+    // RN 0.76+ merged .so init (this solved your previous startup crash)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
   }
 }
